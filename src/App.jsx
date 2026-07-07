f97ff4af97a6b2edcaf25faa8b640d91e59dc07e
Shea <sheae123@gmail.com>
2026-07-06 20:26:15 -0700

    Fixed all the bugs! changed filepath for image, linked App into main, changed website name

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import "./App.css"
+
 function App() {
   return (
     <div className="container">
     <h1 className="title">Yosemite National Park</h1>
-    <img className="scenery-img-styled" src="./scenery.jpg" alt="A beautiful scenery" />
+    <img className="scenery-img-styled" src="./scenery.jpeg" alt="A beautiful scenery" />
       <p className="description">
         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
       </p>
